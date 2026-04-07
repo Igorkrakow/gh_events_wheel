e75b45dcd0eb6d20a8426829288c2e0b4c11aa57
Igorkrakow <56430964+Igorkrakow@users.noreply.github.com>
2026-04-07 13:50:18 +0300

    Test CI with terraform 4

--- terraform/main.tf
+++ terraform/main.tf
@@ -313,4 +313,18 @@ resource "databricks_job" "bootstrap_job" {
       whl = "${var.whl_base_path}/${var.whl_file_name}"
     }
   }
+    task {
+    task_key = "silver_tables_creation_task_test"
+    existing_cluster_id = var.cluster_id
+
+    python_wheel_task {
+      package_name = "app"
+      entry_point  = "main"
+      parameters   = ["silver_tables_creation"]
+    }
+
+    library {
+      whl = "${var.whl_base_path}/${var.whl_file_name}"
+    }
+  }
 }
